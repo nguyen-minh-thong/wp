264d74dcf1a5a6c5a9c48fd8232feea3002fc418
Nguyen Minh Thong <minhthongtuvan@gmail.com>
2019-08-31 12:19:41 +0700

    add header info

---
+++ tho_post_wg.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: Tho_post_wg
+ * Plugin Uri: https://nguyenminhthong.com
+ * Description: A widget to display your own custom post.
+ * Author: Nguyen Minh Thong
+ * Author Uri: https://nguyenminhthong.com
+ * Version: 1.0
+ * /
+  
